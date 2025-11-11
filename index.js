798a7eb0fcdd6802468e9d015906168c4d3357f5
Abdur55 <mrrahim972@gmail.com>
2025-11-11 14:33:54 +0600

    added .env file

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const cors = require('cors');
+require('dotenv').config()
+const cors = require('cors')
 const port = process.env.PORT || 3000
 
+const admin = require("firebase-admin");
 
-// xUHPmW3HeNFkrToY
-// B12_A10_DB
+const serviceAccount = require("path/to/serviceAccountKey.json");
 
-const uri = "mongodb+srv://B12_A10_DB:xUHPmW3HeNFkrToY@cluster0.9gmdrlc.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9gmdrlc.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -20,6 +21,14 @@ const client = new MongoClient(uri, {
 
 app.use(cors())
 app.use(express.json())
+
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
 
 async function run() {
     try {
